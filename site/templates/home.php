2f26fe26b94ede948bb2d1aaef3f63fbc972e125
Dimitris <dkottas@yahoo.com>
2019-09-11 11:58:42 +0300

    new content, homepage news

--- site/templates/home.php
+++ site/templates/home.php
@@ -7,14 +7,15 @@
         </div>
     </article>
 <?php $newsitem = $pages->find('news')->children()->sortBy('date', 'desc')->first();?>
-    <a class="w-full lg:w-2/3 xl:w-1/2 gutter block" href="<?= $newsitem->url() ?>">
+    <div class="w-full lg:w-2/3 xl:w-1/2 gutter">
         <div class="homebox mb-12 p-6">
-            <h2 class="font-bold text-base md:text-lg mb-4 text-teal-500"><?= $newsitem->title() ?></h2>
-            <div class="text-white text-base md:text-lg">
-                <?= $newsitem->text()->excerpt(150) ?>
+            <h2 class="font-bold text-base md:text-lg mb-4 ">
+                <a href="<?= $newsitem->url() ?>" class="blueline"><?= $newsitem->title() ?></h2></a>
+            <div class="text-base hometext">
+                <?= $newsitem->text()->kt() ?>
             </div>
         </div>
-    </a>
+    </div>
 
 </div>
 
