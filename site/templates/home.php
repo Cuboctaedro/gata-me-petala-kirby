dd99c9373c000f1fe31f5388f362e22bab00e530
dkottas <dkottas@nelios.com>
2020-04-05 12:42:26 +0300

    aggelis

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,7 +9,7 @@
 <?php $newsitem = $pages->find('news')->children()->sortBy('date', 'desc')->first();?>
     <div class="w-full lg:w-2/3 xl:w-1/2 gutter">
         <div class="homebox mb-12 p-6">
-            <h2 class="font-bold text-base md:text-lg mb-4 ">
+            <h2 class="uppercase tracking-wider text-base md:text-xl mb-4 ">
                 <a href="<?= $newsitem->url() ?>" class="blueline"><?= $newsitem->title() ?></h2></a>
             <div class="text-base hometext">
                 <?= $newsitem->text()->kt() ?>
